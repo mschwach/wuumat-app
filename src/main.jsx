10e1bf16ffb65787041761a80ed795121cbda52c
mschwach <mat.schwach@gmail.com>
2025-08-06 16:28:58 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // Datei: src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./app.jsx";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
